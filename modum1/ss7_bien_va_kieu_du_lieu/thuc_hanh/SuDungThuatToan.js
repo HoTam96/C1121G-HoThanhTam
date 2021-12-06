1a01b369bd503d91c2da7bdc58b9797491cd9261
Your Name <hotam196@gmail.com>
2021-12-06 17:31:13 +0700

    javascrip

---
+++ modum1/ss7_bien_va_kieu_du_lieu/thuc_hanh/SuDungThuatToan.js
@@ -0,0 +1,6 @@
+let inputWidth = prompt("nhập chiều rộng hình chữ nhật (m)")
+inputWidth = parseInt(inputWidth);
+let inputHeight = prompt(" nhập chiều dài hình chữ nhật (m)")
+inputHeight = parseInt(inputHeight);
+let area = inputWidth*inputHeight
+document.write("Diện tích hình chử nhật là: "+area+" m2")
