26cedb11db0096c4bf5651fd8fd4433b82c6489f
Your Name <hotam196@gmail.com>
2022-03-09 17:50:13 +0700

    update

---
+++ module3/src/ss9_jsp_servlet/thuc_hanh/ung_dung_chuyen_doi_tien_te/src/main/java/ChuyenDoiTienTe.java
@@ -0,0 +1,32 @@
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@javax.servlet.annotation.WebServlet(name = "ChuyenDoiTienTe", urlPatterns = "/convert")
+public class ChuyenDoiTienTe extends HttpServlet {
+
+
+
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+        float rate = Float.parseFloat(request.getParameter("rate"));
+        float usd = Float.parseFloat(request.getParameter("usd"));
+
+        float vnd = rate * usd;
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Rate: " + rate + "</h1>");
+        writer.println("<h1>USD: " + usd + "</h1>");
+        writer.println("<h1>VND: " + vnd + "</h1>");
+        writer.println("</html>");
+
+
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
